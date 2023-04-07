8ac8866948a080921595754fd1ced6261807393a
Addai-Mensah <mensahaddaai@gmail.com>
2023-04-07 15:37:20 +0100

    button

--- src/components/connect/button.jsx
+++ src/components/connect/button.jsx
@@ -5,38 +5,11 @@ import {useState, useEffect} from "react"
 
 
 function Btns() {
-        const [isOpen, setIsOpen] = useState(false);
-        const [isSubModalOpen, setIsSubModalOpen] = useState(false);
-      
-        const handleOpenModal = () => {
-          setIsOpen(true);
-        };
-      
-        const handleCloseModal = () => {
-          setIsOpen(false);
-        };
-      
-        const handleOpenSubModal = () => {
-          setIsSubModalOpen(true);
-        };
-      
-        const handleCloseSubModal = () => {
-          setIsSubModalOpen(false);
-        };
-      
+       
     
   return (
     <div>
-          <button onClick={handleOpenModal}>Open Modal</button>
-      <Modal isOpen={isOpen} onRequestClose={handleCloseModal}>
-        <h2>Main Modal</h2>
-        <button onClick={handleOpenSubModal}>Open Sub-Modal</button>
-        <Modal isOpen={isSubModalOpen} onRequestClose={handleCloseSubModal}>
-          <h2>Sub-Modal</h2>
-          <button onClick={handleCloseSubModal}>Close Sub-Modal</button>
-        </Modal>
-        <button onClick={handleCloseModal}>Close Modal</button>
-      </Modal>
+         <p>Hello</p>
 
     </div>
   )
